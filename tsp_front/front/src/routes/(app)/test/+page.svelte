593281a999813efab79d2bda44e7592a49af9359
park changyong <ridebus82@nate.com>
2024-07-03 12:41:38 +0900

    240703 asldjfgogogogo

--- tsp_front/front/src/routes/(app)/test/+page.svelte
+++ tsp_front/front/src/routes/(app)/test/+page.svelte
@@ -1,32 +1,35 @@
 <script>
     import { browser } from "$app/environment";
-    if (browser) {
-        const editor = SUNEDITOR.create(
-            document.getElementById("editor") || "editor",
-            {
-                // All of the plugins are loaded in the "window.SUNEDITOR" object in dist/suneditor.min.js file
-                // Insert options
-                // Language global object (default: en)
-                lang: SUNEDITOR_LANG["ko"],
-                width: "100%",
-                height:'500px',
-                buttonList: [
-                    ["undo", "redo"],
-                    ["font", "fontSize", "formatBlock"],
-                    [
-                        "bold",
-                    ],
-                    ["fontColor", "hiliteColor", "textStyle"],
-                    "/", // Line break
-                    ["align", "horizontalRule", "list", "lineHeight"],
-                    ["table", "link", "image", "video", "audio" /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
-                    /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
-                    ["fullScreen", "showBlocks", "codeView"],
-                    /** ['dir', 'dir_ltr', 'dir_rtl'] */ // "dir": Toggle text direction, "dir_ltr": Right to Left, "dir_rtl": Left to Right
-                ],
+    import { onMount } from "svelte";
+
+    let editor;
+
+    onMount(() => {
+        editor = SUNEDITOR.create("test", {
+            // All of the plugins are loaded in the "window.SUNEDITOR" object in dist/suneditor.min.js file
+            // Insert options
+            // Language global object (default: en)
+            lang: SUNEDITOR_LANG["ko"],
+            width: "100%",
+            minWidth: 400,
+            height: "500px",
+
+            buttonList: [
+                ["undo", "redo", "font", "fontSize", "formatBlock"],
+                ["bold"],
+                ["fontColor", "hiliteColor", "textStyle"],
+                // "/", // Line break
+                ["align", "horizontalRule", "list", "lineHeight"],
+                ["table", "link", "image", "video" /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
+                /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
+                ["fullScreen", "showBlocks", "codeView"],
+                /** ['dir', 'dir_ltr', 'dir_rtl'] */ // "dir": Toggle text direction, "dir_ltr": Right to Left, "dir_rtl": Left to Right
+            ],
+            callBackSave: function (contents, isChanged) {
+                console.log(contents);
             },
-        );
-    }
+        });
+    });
 </script>
 
 <svelte:head>
@@ -45,7 +48,15 @@
     ></script>
 </svelte:head>
 
-<textarea id="editor">Hi</textarea>
+<textarea id="test">Hi</textarea>
+
+<button
+    on:click={() => {
+        console.log(editor.getContents());
+    }}
+>
+    gogogo
+</button>
 
 <style>
 </style>
